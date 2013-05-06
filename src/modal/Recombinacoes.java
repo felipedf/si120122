6fbf551d6a00b66770509a6a38f90d27ebb9cc9b
Felipe Diogenes <felipe.diogenes13@gmail.com>
2013-05-06 01:31:25 -0300

    Push

--- src/modal/Recombinacoes.java
+++
@@ -1,9 +0,0 @@
-package modal;
-
-import java.util.List;
-
-public interface Recombinacoes {
-	List<String> recombina(List<String> texto, int line);
-	
-	int linhaRecombinada(List<String> texto);
-}
